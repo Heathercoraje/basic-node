7bf0bb5ef48719467fd897b2382c37a0aa6b2c29
heathercoraje <heathercoraje@gmail.com>
2018-04-25 00:21:16 +0900

    basic crud tutorial complete!

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # basic-node
+
+Basic CRUD with node mysql module
+Why not practice CRUD by
+
+* CREATE table and INSERT your past ex lovers
+* READ a list of ex lovers
+* UPDATE table by adding more ex lovers
+* DELETE by clicking delete button and execute query
+
+I found it fun, especially when I click `delete` button.
--- database_mysql.js
+++ database_mysql.js
@@ -8,44 +8,44 @@ var conn = mysql.createConnection({
 
 conn.connect();
 
-// example1
-// const sql = 'SELECT * FROM topic';
-// // rows is array of data (each data is called RowDataPacket)
-// // fields is also array (fields = columns) (each data is called FieldPacket)
-// conn.query(sql, (err, rows, fields) => {
-// 	if (err) {
-// 		console.log(err);
-// 	} else {
-// 		console.log('rows : ', rows);
-// 		console.log('fields : ', fields);
-// 	}
-// });
+// example1 read
+// rows is array of data (each data is called RowDataPacket)
+// fields is also array (fields = columns) (each data is called FieldPacket)
+const sql = 'SELECT * FROM topic';
+conn.query(sql, (err, rows, fields) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('rows : ', rows);
+		console.log('fields : ', fields);
+	}
+});
 
 // example2 insert with params
-// const sql = 'INSERT INTO topic (title, description, author) VALUES(?, ?,?)';
-// const params = [
-// 	'mysql',
-// 	'one of biggest open source relational DB',
-// 	'heathercoraje'
-// ];
-// conn.query(sql, params, (err, results, fields) => {
-// 	if (err) {
-// 		console.log(err);
-// 	} else {
-// 		console.log('heather I am here!', results.insertId);
-// 	}
-// });
+const sql = 'INSERT INTO topic (title, description, author) VALUES(?, ?,?)';
+const params = [
+	'mysql',
+	'one of biggest open source relational DB',
+	'heathercoraje'
+];
+conn.query(sql, params, (err, results, fields) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('heather I am here!', results.insertId);
+	}
+});
 
 // example 3 update
-// const sql = 'UPDATE topic SET title=?, author=? WHERE id=?';
-// const params = ['npm', 'nichee', 2];
-// conn.query(sql, params, (err, results, fields) => {
-// 	if (err) {
-// 		console.log(err);
-// 	} else {
-// 		console.log('heather I am here!', results);
-// 	}
-// });
+const sql = 'UPDATE topic SET title=?, author=? WHERE id=?';
+const params = ['npm', 'nichee', 2];
+conn.query(sql, params, (err, results, fields) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('heather I am here!', results);
+	}
+});
 
 // example 4 delete
 const sql = 'DELETE FROM topic WHERE id=?';
@@ -57,4 +57,6 @@ conn.query(sql, params, (err, results, fields) => {
 		console.log('heather I am here!', results);
 	}
 });
+
+// bye db see you later
 conn.end();
